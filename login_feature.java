2f72af565c31c803d1411d6a1257e975b960484c
CallMeAraaa <103560391+CallMeAraaa@users.noreply.github.com>
2023-10-25 00:56:00 +0700

    Add files via upload

---
+++ login_feature.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class login_feature
+
+{
+
+    Feature: Login functionality
+        Given user is on swag lab website
+    When user enter user id and password
+    and user click login
+        Then the system will navigate user to the main page
+}
